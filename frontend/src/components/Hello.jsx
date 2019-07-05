554b394a0825b63c756c679b825dd3693ac61c4c
seonghee yu <zhdzhdgl@gmail.com>
2019-07-05 11:33:04 +0900

    190705 11:32 Hello.jsx hahaha 추가

--- frontend/src/components/Hello.jsx
+++ frontend/src/components/Hello.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Hello =(props)=>{
-    return <p>Hello, my name is {props.name}</p>
+    return <p>Hello, my name is hahaha{props.name}</p>
 }
 
 export default Hello
