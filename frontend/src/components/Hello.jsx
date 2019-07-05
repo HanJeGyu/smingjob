21a848ef92f16ff7a119a9ea51a437790b7a6766
hjg9363@gmail.com <hjg9363@gmail.com>
2019-07-05 11:20:42 +0900

    first commit

---
+++ frontend/src/components/Hello.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Hello =(props)=>{
+    return <p>Hello, my name is {props.name}</p>
+}
+
+export default Hello
---
+++ frontend/src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/css/App.css';
+import Hello from '../components/Hello'
+
+function App() {
+  return (
+    <div className='wrapper'>
+        <Hello name='smingjob'></Hello>
+      </div>
+  );
+}
+
+export default App;
