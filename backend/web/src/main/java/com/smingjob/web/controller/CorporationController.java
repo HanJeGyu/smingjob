48c4dcefcbf3b25d61da21aa77e96b1ad0e1dd87
hjg9363@gmail.com <hjg9363@gmail.com>
2019-07-11 12:31:44 +0900

    패키지 추가

--- backend/web/src/main/java/com/smingjob/web/controller/CorporationController.java
+++ backend/web/src/main/java/com/smingjob/web/controller/CorporationController.java
@@ -5,14 +5,18 @@ import com.smingjob.web.repositories.CorporationRepository;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * CorporationController
  */
 @RestController
+@RequestMapping("/Corporation")
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class CorporationController {
 
     @Autowired CorporationDTO corDTO;
