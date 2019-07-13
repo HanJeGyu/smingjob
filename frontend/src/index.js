79f68fa6baa76b59e49375bf72c3d3ea6855f0fd
hjg9363@gmail.com <hjg9363@gmail.com>
2019-07-13 16:21:27 +0900

    redux-store base

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,20 @@ import './App.css'
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* 스토어, 라우터 추가 */
+import {Provider} from 'react-redux'
+import {ConnectedRouter} from 'connected-react-router'
+import configureStore, {history} from './stores'
+
+const store = configureStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App />
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+);
 
 serviceWorker.unregister();
