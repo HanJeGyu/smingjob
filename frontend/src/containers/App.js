3cb4ccf2ea4417d3142d2a14eeb0f5ef39c76fcf
seonghee yu <zhdzhdgl@gmail.com>
2019-07-13 14:28:49 +0900

    190713 Material-Ul npm install

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -3,13 +3,14 @@ import '../App.css';
 import Hello from '../components/Hello'
 import Navbar from '../nav/Nav'
 import Header from '../header/Header'
+import Navbar2 from '../nav2/Nav'
 
 function App() {
   return (
     <div className='wrapper'>
          <Header />
       <Navbar />
-   
+      <Navbar2/>
       <Hello name='smingjob'></Hello>
     </div>
   );
