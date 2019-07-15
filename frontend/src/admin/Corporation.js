54fb3daf401dd3b8b51582609c67f345f3ca04fb
seonghee yu <zhdzhdgl@gmail.com>
2019-07-15 20:47:57 +0900

    관리자 개인/기업 회원 관리 페이지

--- frontend/src/admin/Corporation.js
+++ frontend/src/admin/Corporation.js
@@ -9,75 +9,66 @@ export default function Corporation() {
             margin: "100px 50px"
         }
 
-     })); 
+    }));
 
-     const classes = useStyles();
-  const [state, setState] = React.useState({
-    columns: [
-      { title: 'Name', field: 'name' },
-      { title: 'Surname', field: 'surname' },
-      { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-      {
-        title: 'Birth Place',
-        field: 'birthCity',
-        lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-      },
-    ],
-    data: [
-      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-      {
-        name: 'Zerya Betül',
-        surname: 'Baran',
-        birthYear: 2017,
-        birthCity: 34,
-      },
-      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+    const classes = useStyles();
+    const [state, setState] = React.useState({
+        columns: [
+            { title: '기업id', field: 'corId' },
+            { title: '기업명', field: 'name' },
+            { title: '산업/직군', field: 'area' },
+            { title: '인사담당자명', field: 'pmName'},
+            { title: '전화번호', field: 'pmPhone' },
+            { title: '회사위치', field: 'city' },
+            { title: '가입일', field: 'dateJoin' }
+        ],
+        data: [
+            { corId: 'kakao', name: '카카오', area: '포털 및 기타 인터넷 정보매개 서비스업', pmName: '이신혜', pmPhone: '0233451039', city: '성남 분당구', dateJoin: '20170715' },
+            { corId: 'woowahan', name: '우아한형제들', area: '모바일·APP', pmName: '주민호', pmPhone: '0263982231', city: '서울 송파구', dateJoin: '20171008' },
+            { corId: 'nhn', name: 'NHN', area: '유선 온라인 게임 소프트웨어 개발 및 공급업', pmName: '임주아', pmPhone: '0258834655', city: '성남 분당구', dateJoin: '20180125' },
+            { corId: 'hsep', name: '한성기업', area: '식품가공', pmName: '이신형', pmPhone: '0285597645', city: '서울 송파구', dateJoin: '20180310' },
+            { corId: 'tway', name: '티웨이항공', area: '항공 여객 운송업', pmName: '김수미', pmPhone: '0216593347', city: '서울 강남구', dateJoin: '201803411' },
+            { corId: 'gsretail', name: 'GS리테일', area: '백화점·유통·도소매', pmName: '서수호', pmPhone: '0289967411', city: '서울 강남구', dateJoin: '20180506' },
 
-    ],
-  });
+        ],
+    });
 
-  return (
-      <div className={classes.table} >
-    <MaterialTable
-      title="기업회원 관리"
-      columns={state.columns}
-      data={state.data}
-      editable={{
-        onRowAdd: newData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              const data = [...state.data];
-              data.push(newData);
-              setState({ ...state, data });
-            }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              const data = [...state.data];
-              data[data.indexOf(oldData)] = newData;
-              setState({ ...state, data });
-            }, 600);
-          }),
-        onRowDelete: oldData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              const data = [...state.data];
-              data.splice(data.indexOf(oldData), 1);
-              setState({ ...state, data });
-            }, 600);
-          }),
-      }}
-    />
-    </div>
-  );
+    return (
+        <div className={classes.table} >
+            <MaterialTable
+                title="기업회원 관리"
+                columns={state.columns}
+                data={state.data}
+                editable={{
+/*                     onRowAdd: newData =>
+                        new Promise(resolve => {
+                            setTimeout(() => {
+                                resolve();
+                                const data = [...state.data];
+                                data.push(newData);
+                                setState({ ...state, data });
+                            }, 600);
+                        }), */
+/*                     onRowUpdate: (newData, oldData) =>
+                        new Promise(resolve => {
+                            setTimeout(() => {
+                                resolve();
+                                const data = [...state.data];
+                                data[data.indexOf(oldData)] = newData;
+                                setState({ ...state, data });
+                            }, 600);
+                        }), */
+                    onRowDelete: oldData =>
+                        new Promise(resolve => {
+                            setTimeout(() => {
+                                resolve();
+                                const data = [...state.data];
+                                data.splice(data.indexOf(oldData), 1);
+                                setState({ ...state, data });
+                            }, 600);
+                        }),
+                }}
+            />
+        </div>
+    );
 }
