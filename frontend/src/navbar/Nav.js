288c5acd4427e5a1ab62773ae0bfa875f2449879
hjg9363@gmail.com <hjg9363@gmail.com>
2019-07-15 19:38:53 +0900

    소스 정리

--- frontend/src/navbar/Nav.js
+++ frontend/src/navbar/Nav.js
@@ -1,28 +1,17 @@
 import React from "react";
 import clsx from "clsx";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
+import { makeStyles, useTheme, Drawer, CssBaseline, AppBar, 
+        List, Typography, Divider, IconButton, ListItem, 
+        ListItemText, MenuItem, ListItemIcon,
+        Menu, Button, ListSubheader, Toolbar } 
+        from "@material-ui/core";
+import { red, deepPurple, blue } from "@material-ui/core/colors";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
 import InboxIcon from "@material-ui/icons/MoveToInbox";
 import MailIcon from "@material-ui/icons/Mail";
-import { red, deepPurple, blue } from "@material-ui/core/colors";
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import Button from '@material-ui/core/Button';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import AssignmentIcon from '@material-ui/icons/Assignment';
 
 const drawerWidth = 240;
