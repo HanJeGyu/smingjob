5774b3ec628a61754ed663a2ddc92e0e54c10fa3
lhm73012 <lhm73012@gmail.com>
2019-07-16 20:37:02 +0900

    사용자-공고리스트(notice) 서버단 연결(read)..페이징은 아직.

--- frontend/src/notice/Notice.js
+++ frontend/src/notice/Notice.js
@@ -18,110 +18,103 @@ import Divider from '@material-ui/core/Divider';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import DirectionsIcon from '@material-ui/icons/Directions';
+import axios from 'axios'
 
-const useStyles = makeStyles(theme => ({
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    backgroundColor: '#e3ecf4'
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  root: {
-    flexShrink: 0,
-    color: theme.palette.text.secondary,
-    marginLeft: theme.spacing(2.5),
-    textAlign:'center',
-    marginTop: '30px'
-  },
-  searching: {
-    padding: '2px 4px',  
-    display: 'flex',
-    alignItems: 'center',
-    width: '100%',
-    textAlign:'center',
-    margin:'auto',
-    marginTop: '30px',      
-  },
-  input: {
-    marginLeft: 8,
-    flex: 1,
-  },
-  iconButton: {
-    padding: 10,
-  },
-  divider: {
-    width: 1,
-    height: 28,
-    margin: 4,
-  },
-  area:{
-    color: '#404040',
-    
-  },
-  title:{
-    textAlign: 'center',
-    color: '#336699 ',
-    fontWeight: 'bold'
-  },
-  state : {
-    textAlign: 'right',
-    color: '#ba3838',
-    fontWeight: 'bold'
-  },
-  tags: {
-    textAlign: 'center',
-    color: 'SteelBlue '
-  }
 
+export default class Notice extends React.Component{
  
-}));
-
-const cards = [1, 2, 3, 4, 5, 6];
-
-export default function Album(props) {
-  const classes = useStyles();
-  const theme = useTheme();
-  const { count, page, rowsPerPage, onChangePage } = props;
-
-  function handleFirstPageButtonClick(event) {
-    onChangePage(event, 0);
+  state={
+    notices:[]
   }
-
-  function handleBackButtonClick(event) {
-    onChangePage(event, page - 1);
+  componentDidMount(){
+    axios.get('http://localhost:9000/notices')
+    .then(res=>{
+      const notices = res.data;
+      this.setState({notices});
+      
+  })
   }
-
-  function handleNextButtonClick(event) {
-    onChangePage(event, page + 1);
-  }
-
-  function handleLastPageButtonClick(event) {
-    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  }
-  return (
+  
+  render(){
+   /*  let state=this.state;    */  
+    let notices = this.state.notices;
+    let cardGrid ={
+      paddingTop: '5%',
+      paddingBottom: '5%',
+    }
+    let  card= {
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      backgroundColor: '#e3ecf4'
+    }
+    let cardMedia= {
+      paddingTop: '56.25%', // 16:9
+    }
+    let cardContent= {
+      flexGrow: 1,
+    }
+    let root= {
+      flexShrink: 0,
+      //color: palette.text.secondary,
+      marginLeft: '2.5%',
+      textAlign:'center',
+      marginTop: '30px'
+    }
+    let searching= {
+      padding: '2px 4px',  
+      display: 'flex',
+      alignItems: 'center',
+      width: '100%',
+      textAlign:'center',
+      margin:'auto',
+      marginTop: '30px',      
+    }
+    let input= {
+      marginLeft: 8,
+      flex: 1,
+    }
+    let iconButton= {
+      padding: 10,
+    }
+    let divider= {
+      width: 1,
+      height: 28,
+      margin: 4,
+    }
+    let area ={
+      color: '#404040',
+      
+    }
+    let title ={
+      textAlign: 'center',
+      color: '#336699 ',
+      fontWeight: 'bold'
+    }
+    let state = {
+      textAlign: 'right',
+      color: '#ba3838',
+      fontWeight: 'bold'
+    }
+    let tags= {
+      textAlign: 'center',
+      color: 'SteelBlue '
+    }
+  
+  return(  
     <React.Fragment>
-          {/*검색 */}
-          <Container  maxWidth="md">   
-          <Paper className={classes.searching} >
+       {/*검색 */}
+       <Container  maxWidth="md">   
+          <Paper style={searching} >
       {/*    <IconButton className={classes.iconButton} aria-label="Menu">
             <MenuIcon />
           </IconButton> */}
           <InputBase
-            className={classes.input}
+            style={input}
             placeholder="지역. 직무 등 키워드를 입력해주세요."
             inputProps={{ 'aria-label': '검색' }}
           />
-          <IconButton className={classes.iconButton} aria-label="Search">
+          <IconButton style={iconButton} aria-label="Search">
             <SearchIcon />
           </IconButton>
         {/*  <Divider className={classes.divider} />
@@ -131,66 +124,37 @@ export default function Album(props) {
         </Paper>
        </Container>
 
-           {/*메인 */}
-        <Container className={classes.cardGrid} maxWidth="md">
-          <Grid container spacing={6} >
-            {cards.map(card => (
-              <Grid item key={card} xs={12} sm={6} md={4} >
-                <Card className={classes.card}>           
-                  <CardContent className={classes.cardContent}>                        
-                    <Typography className={classes.area} variant="h10" gutterBottom>
-                      직무 - 경력사항
-                    </Typography> 
-                    <Typography><p /></Typography>                       
-                    <Typography  className={classes.title} gutterBottom variant="h4" component="h2" >
-                      기업명
-                    </Typography>
-                    <Typography><p /></Typography>    
-                    <Typography className={classes.tags}  gutterBottom variant="h6" >
-                      #1  #2  #3
-                    </Typography>
-                    <Typography><p /></Typography>    
-                    <Typography  className={classes.state}  >
-                     진행상태
-                    </Typography>
-                   
-                  </CardContent>
-              
-                </Card>
-              </Grid>
-            ))}
-            
-          </Grid>
-          {/*페이징 */}
-          <div className={classes.root}>
-            <IconButton
-              onClick={handleFirstPageButtonClick}
-              disabled={page === 0}
-              aria-label="First Page"
-            >
-              {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-            </IconButton>
-            <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="Previous Page">
-              {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            </IconButton>
-            <IconButton
-              onClick={handleNextButtonClick}
-              disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-              aria-label="Next Page"
-            >
-              {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-            </IconButton>
-            <IconButton
-              onClick={handleLastPageButtonClick}
-              disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-              aria-label="Last Page"
-            >
-              {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-            </IconButton>
-          </div>
-        </Container>
-    
 
-    </React.Fragment>
-  );
+       <Container style ={cardGrid}  maxWidth="md">
+       <Grid container spacing={6} >
+        {this.state.notices.map(notice => <Grid item key={notice} xs={12} sm={6} md={4} >
+            <Card style ={card}>           
+              <CardContent style ={cardContent}>                        
+                <Typography style ={area} variant="h10" gutterBottom>
+                  {notice.area} - {notice.career}
+                </Typography> 
+                <Typography><p /></Typography>                       
+                <Typography style={title} gutterBottom variant="h4" component="h2" >
+                  {notice.corName}
+                </Typography>
+                <Typography><p /></Typography>    
+                <Typography  style ={tags} gutterBottom variant="h6" >
+                  #{notice.tagLocation}  #{notice.tagAttribute}  #{notice.tagArea}
+                </Typography>
+                <Typography><p /></Typography>    
+                <Typography style ={state} >
+                {notice.state}
+                </Typography>
+              
+              </CardContent>
+          
+            </Card>           
+             </Grid>
+              )}
+              </Grid>
+              </Container>
+              </React.Fragment>
+
+  )
+}
 }
