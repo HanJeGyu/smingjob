ff3b22b0dcd67fc35ebc6cebf0993749f8f4bcdb
hjg9363@gmail.com <hjg9363@gmail.com>
2019-07-23 09:11:08 +0900

    save example

--- backend/web/src/main/java/com/smingjob/web/controller/ApplicantController.java
+++ backend/web/src/main/java/com/smingjob/web/controller/ApplicantController.java
@@ -3,12 +3,17 @@ package com.smingjob.web.controller;
 import java.util.List;
 import java.util.Map;
 
+import com.smingjob.web.domain.ApplicantDTO;
+import com.smingjob.web.enttites.Applicant;
 import com.smingjob.web.repositories.ApplicantRepository;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,16 +21,25 @@ import org.springframework.web.bind.annotation.RestController;
  * ApplicantController
  */
 @RestController
-@RequestMapping("/applicant")
+@RequestMapping("/applicants")
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class ApplicantController {
 
     @Autowired
     ApplicantRepository repo;
+    @Autowired
+    ModelMapper modelMapper;
 
     /* 개인회원 마이페이지 지원현황 */
     @GetMapping("/noticeList/{itvSeq}")
     public List<Map<String,Object>> noticeList(@PathVariable String itvSeq) {
         return repo.getNoticeList(Long.parseLong(itvSeq));
     }
+
+/*     @PostMapping("")
+    public void tetetet(@RequestBody ApplicantDTO rdto) {
+        System.out.println("itvSeq : " + rdto.getItvSeq());
+        System.out.println("noticeSeq : " + rdto.getNoticeSeq());
+        repo.save(modelMapper.map(rdto, Applicant.class));
+    } */
 }
---
+++ backend/web/src/main/java/com/smingjob/web/domain/ApplicantDTO.java
@@ -0,0 +1,18 @@
+package com.smingjob.web.domain;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+/**
+ * ApplicantDTO
+ */
+@Data
+@Lazy
+@Component
+public class ApplicantDTO {
+
+    private Long applicantSeq, itvSeq, noticeSeq;
+    private String state;
+}
