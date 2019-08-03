a4775392fea7b822ed757087d92d751d762a5cbf
hjg9363@gmail.com <hjg9363@gmail.com>
2019-08-03 15:20:14 +0900

    관리자 공고에서 지원자 승인처리 오류 수정

--- backend-admin/web/src/main/java/com/admin/web/controller/ApplicantController.java
+++ backend-admin/web/src/main/java/com/admin/web/controller/ApplicantController.java
@@ -1,6 +1,5 @@
 package com.admin.web.controller;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,31 +22,22 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:3001", maxAge = 3600)
 public class ApplicantController {
 
-    @Autowired
-    ApplicantRepository repo;
-    @Autowired
-    ModelMapper modelMapper;
-  
+   @Autowired
+   ApplicantRepository repo;
+   @Autowired
+   ModelMapper modelMapper;
 
-    //지원자 리스트 불러오기
-    @GetMapping("/{noticeSeq}")
-    public List<Map<String,Object>> getApplicants(@PathVariable String noticeSeq){
+   //지원자 리스트 불러오기
+   @GetMapping("/{noticeSeq}")
+   public List<Map<String,Object>> getApplicants(@PathVariable String noticeSeq){
       return repo.getApplicantList(Long.parseLong(noticeSeq));
-    }
+   }
     
-    //지원자리스트 대기/승인 상태 변경처리
-    @PutMapping("/{appSeq}")
-   public HashMap<String, String> modify( @PathVariable String appSeq) {   
-       HashMap<String, String> map = new HashMap<>();
-       Applicant entity = repo.findByApplicantSeq(Long.parseLong(appSeq)).get();
-       if(entity.getAppState().equals("승인")){
-          entity.setAppState("대기");
-       }else{
-          entity.setAppState("승인");
-       }
-       repo.save(entity);
-       map.put("result", "SUCCESS");
-      return map;
-   }   
-
+   //지원자리스트 대기/승인 상태 변경처리
+   @PutMapping("")
+   public void modify(@RequestBody Map<String, Object> rdto) {
+      Applicant entity = repo.findById(Long.parseLong(rdto.get("applicantSeq").toString())).get();
+      entity.setAppState(rdto.get("appState").toString());
+      repo.save(entity);
+   }
 } 
